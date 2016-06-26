4f2bba480b45e19ab61fafa16190584fc8f2501f
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-06-26 15:26:59 +0300

    get content from field

--- giveme.py
+++ giveme.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
+import os
+import tkMessageBox
 
-def printer(event):
-    print ("downloading... step 1")
+def start(event):
+    put_dir=ent.get()
+    print ("downloading... step 1, project:",put_dir)
+    global put_dir
     
 root = Tk()
 
 but = Button(root,
              text="Apply", 
              width=10,height=1,
-             bg="white",fg="green")
+             bg="white",fg="green" )
 
-# border for input field
 ent = Entry(root,width=35,bd=3)
 
 lab = Label(root, text="Название проекта",
@@ -19,15 +22,11 @@ lab = Label(root, text="Название проекта",
             bd=10,
             font="Arial 14")
 
-# click left button of mouse - <Button-1>
-# relations (binding) events and function
-but.bind("<Button-1>", printer)
-
+but.bind("<Button-1>", start)
 
 # visibility of button
 lab.pack()
 ent.pack()
-#tex.pack()
 but.pack()
 
 
